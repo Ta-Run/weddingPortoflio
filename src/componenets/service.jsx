539e942649ff7416f06367326ac33c0b3312daeb
Ta-Run <tarunsharma11091999@gmail.com>
2024-09-12 00:49:01 +0530

    add testmonial

--- src/componenets/service.jsx
+++ src/componenets/service.jsx
@@ -153,8 +153,70 @@ const Service = () => {
           <div>Year of Experience</div>
         </div>
         <div className='ml-4 text-2xl'>
-            <div className='text-yellow-600'> 150+ </div>
+          <div className='text-yellow-600'> 150+ </div>
           WEDDINGS COMPLETED SUCCESSFULLY
+        </div>
+      </div>
+
+      <div className="relative text-center mb-6">
+        <h2 className="font-raleway text-[41.89px] font-extrabold leading-[49.18px]">
+          TESTIMONIAL
+        </h2>
+        <div className="w-[159px] mx-auto mt-2 border-t-2 border-[#FCFC04]"></div>
+      </div>
+
+      
+      <div className="relative flex justify-center items-center w-full h-screen">
+        {/* Box */}
+        <div className="box-content h-40 w-96 p-4 border-4 border-gray-300 bg-white flex items-center space-x-4 relative z-0">
+          {/* Image */}
+          <div>
+            <img
+              src={process.env.PUBLIC_URL + '/images/image 14.png'}
+              alt="Service"
+              className="h-full w-auto"
+            />
+          </div>
+          {/* Text */}
+          <div className="text-gray-700">
+            "Captured our special day beautifully, each moment immortalized with artistry. A true professional, grateful for the memories. DAVID WARNER"
+          </div>
+        </div>
+
+        {/* Previous Arrow */}
+        <div className="absolute left-2 md:left-4 lg:left-6 z-10 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </div>
+
+        {/* Next Arrow */}
+        <div className="absolute right-2 md:right-4 lg:right-6 z-10 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
         </div>
       </div>
 
