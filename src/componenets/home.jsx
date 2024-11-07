a84bb6aa41ab0345ea6d7ce514eb6248fd632d30
unknown <rohit_chandel>
2024-11-07 10:12:03 +0530

    Added changes for comp branch

--- src/componenets/home.jsx
+++ src/componenets/home.jsx
@@ -12,9 +12,7 @@ function Home() {
   const [seeAll, setSeeAll] = useState(false);
   const [expanded, setExpanded] = useState(null);
 
-  const images = [
-    `${process.env.PUBLIC_URL}/images/image 19.png`,
-  ];
+  const images = [`${process.env.PUBLIC_URL}/images/image 19.png`];
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
@@ -27,7 +25,6 @@ function Home() {
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
-
 
   // Function to toggle FAQ answers
   const toggleQuestion = (index) => {
@@ -43,7 +40,6 @@ function Home() {
   const toggleSeeAll = () => {
     setSeeAll(!seeAll); // Toggle between true and false
   };
-
 
   const slides = [
     {
@@ -74,54 +70,63 @@ function Home() {
   const faqs = [
     {
       question: "Can You Capture Our Wedding Day From Start to Finish?",
-      answer: "Absolutely! We provide comprehensive coverage of your wedding day, capturing every moment from preparations to the reception."
+      answer:
+        "Absolutely! We provide comprehensive coverage of your wedding day, capturing every moment from preparations to the reception.",
     },
     {
       question: "Do You Offer Engagement Photo Sessions?",
-      answer: "Yes, we offer engagement sessions that allow you to capture beautiful moments before your wedding, providing a great way to get comfortable in front of the camera."
+      answer:
+        "Yes, we offer engagement sessions that allow you to capture beautiful moments before your wedding, providing a great way to get comfortable in front of the camera.",
     },
     {
-      question: "How Do You Handle Different Lighting Conditions During The Wedding?",
-      answer: "We are experienced in working with various lighting conditions, including natural light and indoor venues, ensuring that every photo looks stunning regardless of the environment."
+      question:
+        "How Do You Handle Different Lighting Conditions During The Wedding?",
+      answer:
+        "We are experienced in working with various lighting conditions, including natural light and indoor venues, ensuring that every photo looks stunning regardless of the environment.",
     },
     {
       question: "Can We Customize Our Wedding Photography Package?",
-      answer: "Definitely! We offer customizable packages to fit your specific needs, including options for hours of coverage, number of photographers, and additional services."
+      answer:
+        "Definitely! We offer customizable packages to fit your specific needs, including options for hours of coverage, number of photographers, and additional services.",
     },
     {
       question: "How Many Photos Will We Receive From Our Wedding?",
-      answer: "The number of photos varies by package, but you can expect to receive hundreds of beautifully edited images that capture the essence of your day."
+      answer:
+        "The number of photos varies by package, but you can expect to receive hundreds of beautifully edited images that capture the essence of your day.",
     },
     {
       question: "When Will We Receive Our Wedding Photos?",
-      answer: "Typically, you will receive your edited wedding photos within 4-6 weeks after your wedding day, depending on the season and our current schedule."
+      answer:
+        "Typically, you will receive your edited wedding photos within 4-6 weeks after your wedding day, depending on the season and our current schedule.",
     },
     {
       question: "Do You Provide Wedding Albums?",
-      answer: "Yes, we offer high-quality wedding albums that beautifully showcase your favorite images from the day, designed to last a lifetime."
+      answer:
+        "Yes, we offer high-quality wedding albums that beautifully showcase your favorite images from the day, designed to last a lifetime.",
     },
     {
       question: "Can You Help Us With A Wedding Photography Timeline?",
-      answer: "Absolutely! We can assist in creating a photography timeline that ensures we capture all the key moments of your wedding day."
+      answer:
+        "Absolutely! We can assist in creating a photography timeline that ensures we capture all the key moments of your wedding day.",
     },
   ];
-
 
   return (
     <div>
       <div className="relative w-full h-screen flex hero object-cover object-center p-4">
         <span className="absolute left-5 sm:left-20 top-5 sm:top-12 box-border h-8 w-8 sm:h-16 sm:w-16 border-t-4 sm:border-t-8 border-l-4 sm:border-l-8"></span>
 
-        <div className="inset-x-0 top-[20%] mx-auto text-center p-4" style={{ fontFamily: "Raleway", lineHeight: "78px" }}>
+        <div
+          className="inset-x-0 top-[20%] mx-auto text-center p-4"
+          style={{ fontFamily: "Raleway", lineHeight: "78px" }}
+        >
           <div className="flex flex-col items-center">
             <h1
               className="text-white font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl"
               style={{ lineHeight: "1.2", fontFamily: "Arial, sans-serif" }}
             >
               SOM
-              <span className="inline-block mx-2">
-                SHARMA
-              </span>
+              <span className="inline-block mx-2">SHARMA</span>
             </h1>
 
             <p
@@ -143,7 +148,6 @@ function Home() {
 
         <span className="absolute right-5 sm:right-20 bottom-5 sm:bottom-12 box-border h-8 w-8 sm:h-16 sm:w-16 border-b-4 sm:border-b-8 border-r-4 sm:border-r-8"></span>
       </div>
-
 
       <div className="text-white py-4 mt-10">
         <div className="relative text-center mb-8">
@@ -208,7 +212,6 @@ function Home() {
             </div>
           </div>
 
-
           {/* Next Arrow */}
           <button
             onClick={nextSlide}
@@ -232,13 +235,16 @@ function Home() {
         </div>
 
         <div className="flex justify-center">
-          <button className="lg:w-[233px] lg:h-[74px] border border-[#FCFC04] text-center text-lg font-semibold text-[#FCFC04] bg-transparent hover:bg-[#FCFC04] hover:text-black transition-colors my-20"
-            onClick={() => navigate("/portfolio")}
+          <button
+            className="w-[98px] lg:w-[233px] lg:h-[74px] border border-[#FCFC04] text-center text-lg font-semibold text-[#FCFC04] bg-transparent hover:bg-[#FCFC04] hover:text-black transition-colors my-20"
+            onClick={() => {
+              navigate("/portfolio");
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
           >
             View All
           </button>
         </div>
-
 
         {/* service */}
         <div className="relative text-center pt-6">
@@ -252,7 +258,8 @@ function Home() {
 
         <div className="px-4 md:px-8 lg:px-16 py-12">
           {/* Grid Container */}
-          <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 sm:gap-20 cursor-pointer"
+          <div
+            className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 sm:gap-20 cursor-pointer"
             onClick={() => navigate("/service")}
           >
             {Array(4)
@@ -282,9 +289,13 @@ function Home() {
               ))}
           </div>
         </div>
-        <div className="flex justify-center mt-8">
-          <button className="lg:w-[233px] lg:h-[74px] border border-[#FCFC04] text-center text-lg font-semibold text-[#FCFC04] bg-transparent hover:bg-[#FCFC04] hover:text-black transition-colors"
-            onClick={() => navigate("/service")}
+        <div className="flex justify-center mt-8 px-4">
+          <button
+            className="w-[98px] lg:w-[233px] lg:h-[74px] border border-[#FCFC04] text-center text-lg font-semibold text-[#FCFC04] bg-transparent hover:bg-[#FCFC04] hover:text-black transition-colors"
+            onClick={() => {
+              navigate("/service");
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
           >
             View All
           </button>
@@ -346,23 +357,29 @@ function Home() {
           <h2 className="font-raleway md:text-[41.89px] font-extrabold md:leading-[49.18px]">
             TESTIMONIAL
           </h2>
-          <span className="
+          <span
+            className="
     w-[80px] sm:w-[100px] md:w-[120px] lg:w-[140px] 
     mx-auto mt-2 border-b-2 border-[#FCFC04] 
     block ml-auto sm:ml-[51%]
-  "></span>
+  "
+          ></span>
         </div>
       </div>
 
-      <div id="default-carousel" className="relative mt-10 mx-4 sm:mx-12 mb-4" data-carousel="static">
-
+      <div
+        id="default-carousel"
+        className="relative mt-10 mx-4 sm:mx-12 mb-4"
+        data-carousel="static"
+      >
         {/* Carousel wrapper */}
         <div className="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
           {slides.map((slide, index) => (
             <div
               key={slide.id}
-              className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${index === currentSlide ? "opacity-100" : "opacity-0"
-                }`}
+              className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${
+                index === currentSlide ? "opacity-100" : "opacity-0"
+              }`}
               data-carousel-item
             >
               <div
@@ -382,8 +399,8 @@ function Home() {
                     {slide.label && (
                       <span className="text-white text-md sm:text-lg">
                         Captured our special day beautifully, each moment
-                        immortalized with artistry. A true professional, grateful for
-                        the memories.
+                        immortalized with artistry. A true professional,
+                        grateful for the memories.
                       </span>
                     )}
                   </p>
@@ -397,9 +414,15 @@ function Home() {
         {/* Slider Indicators */}
         <div className="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
           {slides.map((_, index) => (
-            <button key={index} type="button" className={`w-3 h-3 rounded-full ${currentSlide === index ? "bg-yellow-500"
-              : "bg-white"}`} aria-current={currentSlide === index} aria-label={`Slide ${index + 1}`} onClick={() =>
-                goToSlide(index)}
+            <button
+              key={index}
+              type="button"
+              className={`w-3 h-3 rounded-full ${
+                currentSlide === index ? "bg-yellow-500" : "bg-white"
+              }`}
+              aria-current={currentSlide === index}
+              aria-label={`Slide ${index + 1}`}
+              onClick={() => goToSlide(index)}
             ></button>
           ))}
         </div>
@@ -407,8 +430,7 @@ function Home() {
 
       {/* faq */}
       <div className="relative text-center pt-20 mb-6 ">
-        <h2
-          className="font-raleway md:text-[41.89px] font-extrabold md:leading-[49.18px]">
+        <h2 className="font-raleway md:text-[41.89px] font-extrabold md:leading-[49.18px]">
           F
           <span className="w-[100px] md:w-[159px] mx-auto mt-2 border-b-2 border-[#FCFC04]">
             AQ
@@ -418,7 +440,10 @@ function Home() {
 
       <div className="max-w-6xl mx-auto pt-10 px-4">
         {faqs.slice(0, seeAll ? faqs.length : 4).map((faq, index) => (
-          <div key={index} className="border border-gray-600 mb-4 rounded-md overflow-hidden">
+          <div
+            key={index}
+            className="border border-gray-600 mb-4 rounded-md overflow-hidden"
+          >
             <div
               className="flex justify-between items-center cursor-pointer p-4"
               onClick={() => toggleQuestion(index)}
@@ -426,8 +451,9 @@ function Home() {
               <h3 className="text-lg font-semibold">{faq.question}</h3>
               {/* Icon with rotation transition */}
               <div
-                className={`transform transition-transform duration-300 ${expanded === index ? "rotate-45" : "rotate-0"
-                  }`}
+                className={`transform transition-transform duration-300 ${
+                  expanded === index ? "rotate-45" : "rotate-0"
+                }`}
               >
                 {expanded === index ? (
                   <FaTimes className="text-white" />
@@ -439,8 +465,11 @@ function Home() {
 
             {/* Collapsible answer with transition */}
             <div
-              className={`transition-all duration-500 ease-in-out overflow-hidden ${expanded === index ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
-                }`}
+              className={`transition-all duration-500 ease-in-out overflow-hidden ${
+                expanded === index
+                  ? "max-h-96 opacity-100"
+                  : "max-h-0 opacity-0"
+              }`}
             >
               <hr className="border-t border-gray-600 mx-4" />
               <div className="px-4 pb-4 pt-2 text-gray-400 text-base leading-relaxed text-left">
@@ -461,7 +490,6 @@ function Home() {
           </button>
         </div>
       </div>
-
 
       {/* glimpiss of instagram */}
       <div className="relative text-center pt-20 mb-6">
